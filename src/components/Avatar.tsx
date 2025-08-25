83b98278dec8ef8419745a7c996851d62f3f0dbd
Bachtiar Dwi Pramudi <142956484+superti4r@users.noreply.github.com>
2025-08-25 14:53:36 +0700

    ⚠️ | Disable download logo

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import Image from "next/image";
 
 export default function Avatar({ src, alt }: { src: string; alt: string }) {
     return (
-        <div className="w-40 sm:w-48 md:w-56 lg:w-64 mx-auto">
-            <Image src={src} alt={alt} width={512} height={512} priority className="h-auto w-full" />
+        <div
+            className="w-40 sm:w-48 md:w-56 lg:w-64 mx-auto select-none"
+            onContextMenu={(e) => e.preventDefault()}
+            draggable={false}
+        >
+            <Image
+                src={src}
+                alt={alt}
+                width={512}
+                height={512}
+                priority
+                className="h-auto w-full pointer-events-none select-none"
+                draggable={false}
+            />
         </div>
     );
 }
